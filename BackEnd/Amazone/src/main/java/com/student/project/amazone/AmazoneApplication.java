6a62eba76d2f954fce4f136cc685ac7dcf431cfa
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2022-04-22 21:07:30 +0700

    sd

---
+++ BackEnd/Amazone/src/main/java/com/student/project/amazone/AmazoneApplication.java
@@ -0,0 +1,13 @@
+package com.student.project.amazone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazoneApplication.class, args);
+    }
+
+}
---
+++ BackEnd/Amazone/src/test/java/com/student/project/amazone/AmazoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.project.amazone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
